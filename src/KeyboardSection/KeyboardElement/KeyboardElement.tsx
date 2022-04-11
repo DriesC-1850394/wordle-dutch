22ee47eb265d86842d0eba15efb7e61d2f054101
DriesC-1850394 <dries_cardinaels@student.uhasselt.be>
2022-04-11 14:49:03 +0200

    :lipstick: Made UI Responsive

--- src/KeyboardSection/KeyboardElement/KeyboardElement.tsx
+++ src/KeyboardSection/KeyboardElement/KeyboardElement.tsx
@@ -2,10 +2,8 @@ const KeyboardElement = ({ char, color, disabled, onClick }: { char: string, col
     const className: string = (char.localeCompare('enter') == 0 || char.localeCompare('delete') == 0) ?
         'KeyboardSpecial' : "KeyboardElement"
 
-    const disableKey: boolean = disabled ? disabled : color.localeCompare("#3d393979") == 0
-
     return (
-        <div className={className} style={{ backgroundColor: color, cursor: disableKey ? 'unset' : 'pointer' }} onClick={disableKey ? undefined : () => onClick(char)}>
+        <div className={className} style={{ backgroundColor: color, cursor: disabled ? 'unset' : 'pointer' }} onClick={disabled ? undefined : () => onClick(char)}>
             {char}
         </div>
     )
